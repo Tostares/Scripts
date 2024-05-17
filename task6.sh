a68131f4bd6bf309a880724c89e70062c8c18254
Rini Kilend <t.kilend@web.de>
2024-05-17 17:12:04 +0200

    Version 1 with error

---
+++ task6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+counter=1
+echo "Hello I am your file creating assistant."
+echo "Where should they be located?"
+read path
+
+#cd $path
+	until [ $input = "no" ]
+do
+	echo "Do you want to create one more file?"
+	read input
+	filename="file$counter.txt"
+	touch "$path/$filename"
+	((counter++))
+	
+done
+#echo "$counter"
+echo "You can check on your files with ls $path"
