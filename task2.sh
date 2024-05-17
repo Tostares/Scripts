7e48092c1776d75a3610b2f9e4be9ae1265f8097
Rini Kilend <t.kilend@web.de>
2024-05-17 13:20:19 +0200

    is it working?

---
+++ task2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Hello, World!"
+echo "Today is: `date`"
+echo "You are logged in as: $USER"
+echo "Your current working directory is: `pwd`"
---
+++ task3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello, what is your name?"
+read NAME
+echo "My name is: $NAME."
