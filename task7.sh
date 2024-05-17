1e69226a93d3d52f60245faba37c34a256db8fb5
Rini Kilend <t.kilend@web.de>
2024-05-17 18:10:40 +0200

    that was doable

--- task7.sh
+++ task7.sh
@@ -3,9 +3,8 @@
 echo "What is the name of the file you are looking for? (please specify file ending)"
 
 read input
-find . -name $input 
 
-if [ $input -eq "$(find . -name $input)" ]
+if [ -f $input ]
 	then
 	       echo "The file $input exists."
        else
